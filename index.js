0f9771e31466ed9630280615024c0439ad7a5fbe
Anthony Gacis (@ageesea) <anthony.gacis@yahoo.com>
2021-09-25 20:56:14 +0800

    Fix code style

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-let getFormValues = (form_selector, isExcludeEmpty = false) => {
-    let p = document.querySelector(form_selector);
-    let form_inputs = p.querySelectorAll("input, select, textarea");
-    let output = {};
-    if (form_inputs.length) {
-        form_inputs.forEach(function (item, index) {
-            let v = item.value;
-            let formName = item.getAttribute("name");
+const getFormValues = (formSelector, isExcludeEmpty = false) => {
+  const p = document.querySelector(formSelector)
+  const formInputs = p.querySelectorAll('input, select, textarea')
+  const output = {}
+  if (formInputs.length) {
+    formInputs.forEach(function (item, index) {
+      const v = item.value
+      const formName = item.getAttribute('name')
 
-            if (formName !== "") {
-                if (isExcludeEmpty && v === "") return;
-                output[formName] = v;
-            }
-        });
-    }
+      if (formName !== '') {
+        if (isExcludeEmpty && v === '') return
+        output[formName] = v
+      }
+    })
+  }
 
-    return output;
+  return output
 }
 
-export { getFormValues };
+export { getFormValues }
